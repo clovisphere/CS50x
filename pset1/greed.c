63d83d805d48e69eee0196bf0b9938aa90431926
clov <clovis.mugaruka@gmail.com>
2020-01-22 06:16:49 +0300

    added mario and cah solutions

---
+++ pset1/greed.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h>
+
+// starts program execution
+int main() 
+{
+    int allowed_change[] = {25, 10, 5, 1}; // holds allowed $ changes
+    float input;
+    int cents, coins = 0;
+    
+    do 
+    {
+        input = get_float("Change owed: "); // user input
+        cents = round(input * 100);
+    } 
+    while (cents < 0); // show prompt if input is negative
+    
+    for (int i = 0; i < 4; i++) 
+    {
+       while (cents >= allowed_change[i]) 
+       {
+           coins++;
+           cents -= allowed_change[i];
+       }
+    }
+ 
+    printf("%d\n", coins);
+}
---
+++ pset1/mario.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include<cs50.h>
+
+// define LOWER and UPPER LIMIT constants
+#define LOWER_LIMIT 1
+#define UPPER LIMIT 8
+
+/**
+ * builds the pyramid with given height.
+ *
+ * @height: the height of the pyramid
+ */
+void build_pyramid(int height) {
+    int i, j;
+    int space = height - 1;
+
+    for (i = 0; i < height; i++) { // outer loop
+        if (space > 0) printf("%*c", space, ' ');
+        // inner loop that prints the bricks
+        for (j = 0; j <= i; j++) {
+            printf("#");
+        }
+        // c hack to print x number of character
+        printf("  %.*s", j, "#########");
+        // add line break before moving to move to the next raw
+        printf("\n");
+        space -= 1;
+    } // end outer loop
+}
+
+// starts program execution
+int main() {
+    puts("Let's build a pyramid..\n");
+    
+    int height;
+    
+    do {
+        height = get_int("Height (value should be between 1 and 8): ");
+    } while (height < 1 || height > 8); 
+    // the magic starts here:-)
+    build_pyramid(height);
+    return 0;
+}
