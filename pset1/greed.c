1c51bd3a56e3e8104f544e70f615ce937fcded8c
clov <clovis.mugaruka@gmail.com>
2020-01-22 07:11:54 +0300

    changed C style

--- pset1/greed.c
+++ pset1/greed.c
@@ -3,27 +3,21 @@
 #include<math.h>
 
 // starts program execution
-int main() 
-{
+int main() {
     int allowed_change[] = {25, 10, 5, 1}; // holds allowed $ changes
     float input;
     int cents, coins = 0;
     
-    do 
-    {
+    do {
         input = get_float("Change owed: "); // user input
         cents = round(input * 100);
-    } 
-    while (cents < 0); // show prompt if input is negative
+    } while (cents < 0); // show prompt if input is negative
     
-    for (int i = 0; i < 4; i++) 
-    {
-       while (cents >= allowed_change[i]) 
-       {
-           coins++;
-           cents -= allowed_change[i];
-       }
+    for (int i = 0; i < 4; i++) {
+        while (cents >= allowed_change[i]) { 
+            coins++;
+            cents -= allowed_change[i];
+        }
     }
- 
     printf("%d\n", coins);
 }
